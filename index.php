ad8fa5c47d4cff31b758b191bff3da313c0ed63a
Golden Chimusinde <golden.chimusinde@cassavasmartech.co.zw>
2021-01-16 13:25:18 +0200

    Uploaded Images For Readme

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Password</title>
-
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet"/>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
